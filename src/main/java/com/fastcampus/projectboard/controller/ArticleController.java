d40d308cfe958071fa346db915eeb65404bb9dee
eJwtzL1uwyAUhuF7YfbgH2xitqpLUjXq0Crphvg5ThHYoIMdN41y70WRt0+f9Lx3IrWGlL6Cg4lwwpTs6pKp3cD6hpmWKsUaSqum1FXX11070F3ZyJYUJGK4WgO40ftWEvOWuvwEcb72+/bz7/jtzofXlzTVR6erU3w/rXRkb7U7fASTU08i5luE7BRIBMxv0uF5SDPaicdFeauFg1uBIA0PeMkjhmJJgBxGaX2xBnSDD2vG8BstQhJyJnxavH88/gGm3E8q <kpwernner@gmail.com>
2023-05-29 14:58:03 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러 테스트.
    그리고 시큐리티 설정을 부를 수 있도록 `@Import` 추가

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -27,7 +27,7 @@ public class ArticleController {
     @GetMapping("/{articleId}") // 1,2,3 이런식으로 articleId가 들어올것인데 이를 {articleId}로 표현
     public String article(@PathVariable Long articleId, ModelMap map){
         // @PathVariable이라는 것은 위의 getmapping로 전달되는 parameter를 articleId라는 parameter로 받겠다는 뜻이다.
-        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터 
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터
         map.addAttribute("articleComments", List.of());
         return "articles/detail";
     }
